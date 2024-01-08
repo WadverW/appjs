d4cd8313a4ce4cd6915c565745154281a76c701c
Wadver <wadverw@gmail.com>
2024-01-08 12:26:27 +0200

    change in indexjs

--- index.js
+++ index.js
@@ -1,2 +1,91 @@
-const num= 50;
-(num == 50) ? console.log(`ok`): console.log(`error`);
+// const num= 50;
+//(num == 50) ? console.log(`ok`): console.log(`error`);
+
+// switch (num) {
+//     case 30:
+//         console.log(`no`);
+//         break;
+//     case 55:
+//         console.log(`no way`);
+//         break;
+//     case 50:
+//         console.log(`right`);
+//         break;
+//     default:
+//         console.log(`not this time`);
+//         break;
+// }
+
+// const hamburger=true;
+// const fries=true;
+
+// if (hamburger && fries){
+//     console.log(`I am fed up`);
+// }
+
+// const hamburger=3;
+// const fries=0;
+
+// console.log((hamburger || fries)); //3
+// console.log((hamburger && fries)); //0
+
+// const hamburger=3;
+// const fries=1;
+
+// if (hamburger===5 && fries){
+//     console.log(`Everyone is fed up`);
+// } else{
+//     console.log(`Lets go away`);
+// }
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//     console.log('Done!')
+//  } else {
+//     console.log(`error`);
+//  }
+ 
+let victoryReport, mariaReport, dinaReport, kateReport = `done`;
+console.log((victoryReport || mariaReport || dinaReport || kateReport)); //done
+
+console.log(!0); //not faulse = true
+
+
+// console.log( NaN || 2 || undefined );
+
+// console.log( NaN && 2 && undefined );
+
+// console.log( 1 && 2 && 3 );
+
+// console.log( !1 && 2 || !3 );
+
+// console.log( 25 || null && !3 );
+
+// console.log( NaN || null || !3 || undefined || 5);
+
+// console.log( NaN || null && !3 && undefined || 5);
+
+// console.log( 5 === 5 && 3 > 1 || 5);
